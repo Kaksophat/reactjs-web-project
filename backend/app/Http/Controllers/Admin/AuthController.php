081fe8e2e36fc638fcf260a8c2c01a9ad0e544f1
kaksophat <kaksophatt@gmail.com>
2025-03-09 10:01:58 +0700

    register and login api

---
+++ backend/app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+
+        return response()->json([
+            'status' =>200 ,
+            'user' => $user
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'status' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        else{
+            $token = $user->createToken('authToken')->plainTextToken; 
+            return response()->json([
+                'status' => 200,
+                'token' => $token,
+                "user" => $user
+            ]);
+        }
+
+     
+    }
+}
